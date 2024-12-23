4618f1bb56262e475b9569ad64ad9f9e6da84574
Deepanjali Dilish <0006deepanjalidilishvft@gmail.com>
2024-12-23 18:40:36 +0530

    inital

--- script.js
+++ script.js
@@ -5,21 +5,10 @@ hamburger.addEventListener('click', () =>{
     navbarCollapse.classList.toggle('show')
 })  
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const hamburger = document.querySelector('.hamber-burger');
-//     const dropdown = document.querySelector('.navbar-collapse');
-//     hamburger.addEventListener('click', function() {
-//         // Toggle the display of the dropdown
-//         if (dropdown.style.display === 'block') {
-//             dropdown.style.display = 'none';
-//         } else {
-//             dropdown.style.display = 'block';
-//         }
-//     });
-//     // Optional: Close the dropdown if clicked outside
-//     window.addEventListener('click', function(event) {
-//         if (!hamburger.contains(event.target) && !dropdown.contains(event.target)) {
-//             dropdown.style.display = 'none';
-//         }
-//     });
-// });
+// const drop = document.querySelector('.dropdown-menu');
+// const navbar = document.querySelector('.navbar-top-collapse')
+
+// drop.addEventListener('click', () =>{
+//     drop.classList.toggle('show')
+// })  
+
