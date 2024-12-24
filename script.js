12c1705db78b9bdf108321a7dc872f45126d22f4
Deepanjali Dilish <0006deepanjalidilishvft@gmail.com>
2024-12-24 13:33:54 +0530

    inital

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const hamburger = document.querySelector('.hamber-burger');
-const navbarCollapse = document.querySelector('.collapse')
+ import data from "./data/categories.json";
 
-hamburger.addEventListener('click', () =>{
-    navbarCollapse.classList.toggle('show')
-})  
 
-// const drop = document.querySelector('.dropdown-menu');
-// const navbar = document.querySelector('.navbar-top-collapse')
-
-// drop.addEventListener('click', () =>{
-//     drop.classList.toggle('show')
-// })  
-
+const hamburger = document.querySelector(".hamber-burger");
+const navbarCollapse = document.querySelector(".collapse");
+const categoriesDropdown = document.getElementById("menu-top");
+const categoryListContainer = document.getElementById("catgories-list"); // Add this for container selection
+hamburger.addEventListener("click", () => {
+  navbarCollapse.classList.toggle("show");
+});
+// Initial visibility state
+let showCategories = false;
+console.log(categories);
+// Populate the dropdown list
+data.forEach((category) => {
+  const categoryList = document.createElement("li");
+  categoryList.textContent = category.value;
+  categoriesDropdown.appendChild(categoryList);
+});
+// Toggle dropdown visibility
+categoryListContainer.addEventListener("click", () => {
+  showCategories = !showCategories;
+  if (showCategories) {
+    categoriesDropdown.classList.add("show-items");
+    categoriesDropdown.classList.remove("hide-items");
+  } else {
+    categoriesDropdown.classList.add("hide-items");
+    categoriesDropdown.classList.remove("show-items");
+  }
+});
